266d1e4676d71173d5a4a6714359bef2ba57c7e8
aamaya <alessandro.amaya5@gmail.com>
2024-02-01 14:00:46 -0600

    feat(utils): add base64Service

--- WolfApiCore/Program.cs
+++ WolfApiCore/Program.cs
@@ -1,4 +1,5 @@
 using WolfApiCore.Hubs;
+using WolfApiCore.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSignalR();
+builder.Services.AddTransient<Base64Service>();
 
 
 string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
---
+++ WolfApiCore/Utilities/Base64Service.cs
@@ -0,0 +1,21 @@
+namespace WolfApiCore.Utilities
+{
+    public class Base64Service
+    {
+        public string DecodeBase64(string base64String) {
+
+            if( string.IsNullOrEmpty(base64String) ) {
+                return string.Empty;
+            }
+
+            byte[] valueBytes = Convert.FromBase64String(base64String);
+            return System.Text.Encoding.UTF8.GetString(valueBytes);
+        }
+
+        public bool IsBase64String(string base64)
+        {
+            Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+            return Convert.TryFromBase64String(base64, buffer , out int bytesParsed);
+        }
+    }
+}
